d0140871a76c23acef8a84130b1e112ee6efc0fb
Michael Frank <michael.frank@qaware.de>
2018-02-07 16:38:29 +0100

    add intro example

---
+++ src/main/java/de/frank/jmh/basic/BadIntroExample.java
@@ -0,0 +1,34 @@
+package de.frank.jmh.basic;
+
+
+import java.util.Arrays;
+
+
+/**
+ * How NOT to benchmark
+ *
+ * @author Michael Frank
+ * @version 1.0 05.12.2016
+ */
+
+public class BadIntroExample {
+    public static void main(String[] args) {
+        final int[] data = new int[10_000_000];
+        Arrays.fill(data, 42);
+        for (int i = 0; i < 10; i++) {
+            long start = System.currentTimeMillis();
+            sum(data);
+            long duration = System.currentTimeMillis() - start;
+            System.out.println("Took: " + duration + "ms");
+        }
+    }
+
+
+    public static int sum(int[] arr) {
+        int sum = 0;
+        for (int i : arr) {
+            sum += i;
+        }
+        return sum;
+    }
+}
