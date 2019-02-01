507ed51bb81e191a2cd0fd4dbcb60aee398cb771
Michael-Frank <michael.frank@qaware.de>
2019-02-01 13:11:27 +0100

    add more description to intro example

--- src/main/java/de/frank/jmh/intro/FlawedBasicBench.java
+++ src/main/java/de/frank/jmh/intro/FlawedBasicBench.java
@@ -3,21 +3,35 @@ package de.frank.jmh.intro;
 import java.util.concurrent.TimeUnit;
 
 /**
- * This demonstrates HOW NOT! TO BENCHMARK in multiple steps and the "improvements" one could think of to make it "right" (but will not work)
- * <p>
- * This is from the live-demo part of the Slides
- * We walk through some issues while benchmarking Java.
- * Mostly optimizations the jvm does - which is good - but bad for getting benchmarkresults.
- * * compile threshold c1/c2
- * * dead-code elimination
- * * constants
- * * statics
+ * "FlawedBasicBench" demonstrates HOW NOT! TO BENCHMARK in Java.
+ *
+ * Leanings:
+ *   - Be aware of the very high abstraction level!
+ *   - use the right tool - don't re-invent the wheel
+ *   - question the results!
+ *   - performance heavily depends on the (input-) data
+ *
+ * Description:
+ * Shows the myriads, misconceptions and ill advises of (micro-) benchmarking a managed language.
+ * Walks through stages of "improvements" one could think of to make it "right", but ultimately will still fail.
+ *
+ * This is from the live-demo part of my Slides "JMH - Micro benchmarking done right"
+ *
+ * In this short demo we touch some optimizations the jvm does - which is good for your program,
+ * but bad for getting meaningful and deterministic benchmark results.
+ *
+ * We will brush on the topics of:
+ *  - compile thresholds in c1/c2 compilers
+ *  - dead-code elimination
+ *  - constants
+ *  - statics
  *
  * @author Michael Frank
  * @version 1.0 05.12.2016
  */
 public class FlawedBasicBench {
 
+    // We want to test 3 different variations of this Algorithm to chose the fastest.
     interface Algo {
         int doWork(int x);
     }
@@ -26,15 +40,16 @@ public class FlawedBasicBench {
         int x = 0;
         Algo[] implementations = new Algo[]{new AlgoImpl1(), new AlgoImpl2(), new AlgoImpl3()};
         for (Algo algo : implementations) {
-            //HOWTO: run each variant at its own (jvm instance)!
-//            singleBenchRun_v1(algo, 1_000);
-//            singleBenchRun_v1(algo, 10_000);
-//            singleBenchRun_v1(algo, 100_000);
-//            singleBenchRun_v1(algo, 1_000_000);
-//            singleBenchRun_v1(algo, 200_000_000);
-//            x += singleBenchRun_v2(algo, 200_000_000);
-//            x += singleBenchRun_v3(algo, 200_000_000);
-            x += singleBenchRun_v4(algo, 200_000_000);
+            //HOWTO: run each variant independently on its own 1..3 times and try to observe and explain
+            singleBenchRun_v1(algo, 100); //extreme startup variance
+//            singleBenchRun_v1(algo, 10_000); //closer but still a lot run to run and in between variance -
+//            singleBenchRun_v1(algo, 100_000);  //Impl1 gets compile (warmup) penalty
+//            singleBenchRun_v1(algo, 1_000_000); // so Impl1 or Impl12 is clearly better then Impl3??
+//            singleBenchRun_v1(algo, 200_000_000);//..somethings wrong, we are getting impossible fast!
+//            x += singleBenchRun_v2(algo, 200_000_000);//lets try fix it ...dang!
+//            x += singleBenchRun_v3(algo, 200_000_000);//lets try fix it ...dang!
+//            x += singleBenchRun_v4(algo, 200_000_000);//better? nope still broken..
+            //and on and on and so on ...
         }
         System.out.println(x);
     }
@@ -98,6 +113,17 @@ public class FlawedBasicBench {
         double throughputPerS = invocations * scale;
         System.out.printf("%s %.3fns/op %.0fops/s%n", algo.getClass().getSimpleName(), nsPerOp, throughputPerS);
     }
+
+
+
+
+
+
+
+
+
+
+
 
 
     static class AlgoImpl1 implements Algo {
